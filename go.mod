83d8fddd5932fc170d5b7887f1f6da2fd0fa39a3
forencen <1q2w3e4r>
2022-03-08 17:18:04 +0800

    29

--- go.mod
+++ go.mod
@@ -2,7 +2,9 @@ module publicChain
 
 go 1.17
 
+require github.com/syndtr/goleveldb v1.0.0
+
 require (
-	github.com/syndtr/goleveldb v1.0.0
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
 	"publicChain/block"
 	"publicChain/pow"
 )
@@ -24,13 +27,44 @@ func AddBlock2Db(bc *block.BlockChain, data string) []byte {
 }
 
 func main() {
-	bc := block.NewBlockChain()
-	defer bc.Db.Close()
+	addBlockCmd := flag.NewFlagSet("addBlock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printChain", flag.ExitOnError)
+
+	flagAddBlockData := addBlockCmd.String("data", "xxxxxx", "区块内容")
+	if len(os.Args) <= 1 {
+		printUsage()
+		return
+	}
+	switch os.Args[1] {
+	case "addBlock":
+		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "printChain":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	default:
+		printUsage()
+		os.Exit(1)
+	}
+	if addBlockCmd.Parsed() {
+		fmt.Println(*flagAddBlockData)
+	}
+
+	//bc := block.NewBlockChain()
+	//defer bc.Db.Close()
 
 	//genesisBlock2Db(bc)
 	//AddBlock2Db(bc, "a to b 100")
 	//AddBlock2Db(bc, "b to a 30")
 	//AddBlock2Db(bc, "c to b 100")
 	//AddBlock2Db(bc, "b to d 10")
-	bc.PrintChain()
+	//bc.PrintChain()
+}
+
+func printUsage() {
+	fmt.Println("this is usage")
 }
